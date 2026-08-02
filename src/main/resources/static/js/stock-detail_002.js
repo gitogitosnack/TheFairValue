01e1baa63ae8f3129ae96581618762bdfe79323c
DESKTOP-O0C8GJT\TA <tsubasa.adachi@outlook.com>
2026-08-02 18:50:59 +0900

    kkkk

--- src/main/resources/static/js/stock-detail_002.js
+++ src/main/resources/static/js/stock-detail_002.js
@@ -3,21 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // シミュレーションパラメータセクション
   // ===================================
 
-  // << WACC(割引率,一年後の100万円)(sliderの設定・動きを単数のみ制御) >>
-  const waccSlider = document.getElementById("wacc-slider");
-  const waccValueDisplay = document.getElementById("wacc-value");
-
-  // 初期状態の数値（HTMLに書いた初期値）を反映させる
-  const waccInitialValue = parseFloat(waccSlider.value);
-  waccValueDisplay.textContent = `${waccInitialValue.toFixed(1)}%`;
-
-  // スライダーが動かされた時に実行する処理
-  waccSlider.addEventListener("input", (event) => {
-    // 現在のスライダーの値を取得し、% をつけて表示を更新
-    const waccCurrentValue = parseFloat(event.target.value);
-    waccValueDisplay.textContent = `${waccCurrentValue.toFixed(1)}%`;
-  });
-
   // << 将来5年間の成長率, 永久成長率(sliderの設定・動きを複数同時に制御) >>
   // 1. ページ内のすべてのスライダーグループを取得
   const groups = document.querySelectorAll(".slider-param-group");
